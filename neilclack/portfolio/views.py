386a887b1b956a4b95903e67ee57e7cd6ae89772
NeilClack <c.neilclack@gmail.com>
2023-02-07 15:04:09 -0600

    Add projects page, view and route

--- neilclack/portfolio/views.py
+++ neilclack/portfolio/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Project
 
 
 
 def index(request):
     return render(request, 'portfolio/index.html')
+
+def projects(request):
+    projects = Project.objects.all()
+    context = {
+        "projects" : projects
+    }
+    return render(request, "portfolio/projects.html", context)
