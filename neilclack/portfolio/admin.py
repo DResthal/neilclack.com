ae4a8250248b6e873963dd2f374f510c3e2ca589
NeilClack <c.neilclack@gmail.com>
2023-02-10 15:13:00 -0600

    Add new models to admin

--- neilclack/portfolio/admin.py
+++ neilclack/portfolio/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Category, Project
+from .models import Category, Project, Resume, Job, Bullet
 
 admin.site.register(Category)
 admin.site.register(Project)
+admin.site.register(Resume)
+admin.site.register(Job)
+admin.site.register(Bullet)
