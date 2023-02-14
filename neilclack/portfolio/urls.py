0eadde3421d8fb71a5e7e704dfc235adf704a2d9
NeilClack <c.neilclack@gmail.com>
2023-02-14 09:50:09 -0600

    renamed experience to resume

--- neilclack/portfolio/urls.py
+++ neilclack/portfolio/urls.py
@@ -2,9 +2,8 @@ from django.urls import path
 from . import views
 
 
-
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('projects/', views.projects, name="projects"),
-    path('experience/', views.experience, name="experience")
+    path("", views.index, name="index"),
+    path("projects/", views.projects, name="projects"),
+    path("resume/", views.resume, name="resume"),
 ]
--- neilclack/portfolio/views.py
+++ neilclack/portfolio/views.py
@@ -2,27 +2,22 @@ from django.shortcuts import render
 from .models import Project, Resume, Job, Bullet
 
 
-
 def index(request):
-    return render(request, 'portfolio/index.html')
+    return render(request, "portfolio/index.html")
+
 
 def projects(request):
     projects = Project.objects.all()
-    context = {
-        "projects" : projects
-    }
+    context = {"projects": projects}
 
     return render(request, "portfolio/projects.html", context)
 
-def experience(request):
+
+def resume(request):
     resume = Resume.objects.get()
-    jobs   = Job.objects.all()
-    bullets= Bullet.objects.all()
-    
-    context = {
-        "resume"  :resume,
-        "jobs"    :jobs,
-        "bullets" :bullets
-    }
-    
-    return render(request, "portfolio/experience.html", context)
+    jobs = Job.objects.all()
+    bullets = Bullet.objects.all()
+
+    context = {"resume": resume, "jobs": jobs, "bullets": bullets}
+
+    return render(request, "portfolio/resume.html", context)
