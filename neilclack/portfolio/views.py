c0b4de70d24cbf7953b500ea85a6ef2e5e8fe2b0
NeilClack <c.neilclack@gmail.com>
2023-02-16 13:08:03 -0600

    Got contact email working.

--- neilclack/portfolio/views.py
+++ neilclack/portfolio/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.conf import settings
 from .models import Project, Resume, Job, Bullet
-from django.http import HttpResponse
+from django.core.mail import send_mail, BadHeaderError
 from .forms import ContactForm
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def index(request):
@@ -26,7 +30,6 @@ def resume(request):
 
 
 def contact(request):
-    form = ContactForm()
     if request.method == "POST":
         form = ContactForm(request.POST)
 
@@ -34,9 +37,28 @@ def contact(request):
             try:
                 form.save()
             except:
+                # I chose to pass here for now because even if I can't save the email, I still want the email to send.
+                # form.is_valid() checks for validation, .save() just records to the database.
+                pass
+
+            email_address = form.cleaned_data["email"]
+
+            subject = f"Portfolio Contact Request from {form.cleaned_data['fname']} {form.cleaned_data['lname']}"
+
+            content = f"""
+            {form.cleaned_data["fname"]} {form.cleaned_data["lname"]}\n
+            {form.cleaned_data["role"]} @ {form.cleaned_data["company"]}\n
+            {form.cleaned_data["message"]}
+            """
+
+            try:
+                send_mail(subject, content, email_address, [settings.EMAIL_HOST_USER])
+                return render(request, "portfolio/thankyou.html")
+            except BadHeaderError:
                 return render(request, "portfolio/formerror.html")
 
         return render(request, "portfolio/formerror.html")
 
+    form = ContactForm()
     context = {"form": form}
     return render(request, "portfolio/contact.html", context)
