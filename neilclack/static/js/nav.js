6ee74be100a9017258b19d1667e2320583a5faf8
NeilClack <c.neilclack@gmail.com>
2023-02-17 14:42:16 -0600

    Updated navbar for larger screens.

--- neilclack/static/js/nav.js
+++ neilclack/static/js/nav.js
@@ -1,7 +1,12 @@
 function toggleNav() {
-    var x = document.getElementById("nav-links");
-    x.className = x.className === 'hide-nav' ? 'show-nav' : 'hide-nav';
+    const width = window.innerWidth || document.documentElement.clientWidth ||
+        document.body.clientWidth;
 
-    var body = document.querySelector('body');
-    body.style.overflow = body.style.overflow === '' ? 'hidden' : '';
+    if (width < 768) {
+        var x = document.getElementById("nav-links");
+        x.className = x.className === 'hide-nav' ? 'show-nav' : 'hide-nav';
+
+        var body = document.querySelector('body');
+        body.style.overflow = body.style.overflow === '' ? 'hidden' : '';
+    }
 } 
